a80ebf01aa581dd6af27f7c2e3d18499e9e5c3e5
Chris Bowen Jr <laylong@gmx.com>
2023-09-03 08:01:41 -0400

    Form Styling Chore:  - Form section dividers

--- src/BuyingForm.jsx
+++ src/BuyingForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { TextField, FormControl, useFormControl } from '@mui/material';
+import { TextField, Divider } from '@mui/material';
 import { Button } from '@mui/base/Button';
 import { Select, MenuItem, InputLabel } from '@mui/material';
 import Paper from '@mui/material/Paper';
@@ -26,7 +26,7 @@ export default function BuyingForm() {
 
     const handleQuantity = (event) => {
         setQuantity(event.target.value);
-        if(/^[1-9]\d*$/.test(event.target.value)) {
+        if (/^[1-9]\d*$/.test(event.target.value)) {
             setBadQuantity(false);
         } else {
             setBadQuantity(true);
@@ -35,7 +35,7 @@ export default function BuyingForm() {
 
     const handleName = (event) => {
         setName(event.target.value);
-        if(event.target.value.length > 2) {
+        if (event.target.value.length > 2) {
             setBadName(false);
         } else {
             setBadName(true);
@@ -44,7 +44,7 @@ export default function BuyingForm() {
 
     const handleCost = (event) => {
         setCost(event.target.value);
-        if(/^\d*.[0-9][0-9]$/.test(event.target.value)) {
+        if (/^\d*.[0-9][0-9]$/.test(event.target.value)) {
             setBadCost(false);
         } else {
             setBadCost(true);
@@ -114,82 +114,82 @@ export default function BuyingForm() {
 
     return (
         <>
-        <Box sx={{ padding: "8px "}}>
-            <h3>Presets</h3>
-            
-            <InputLabel id="preset-select-label">Presets</InputLabel>
-            <Select
-                labelId="preset-select-label"
-                id="preset-purchase"
-                label="Presets"
-                value={""}
-                sx= {{ width: "200px" }}
-                onChange={handlePreset}
-            >
-                <MenuItem value={{quantity: "",name: "Test 1", cost: "", tags: "operations"}}>
-                    Test 1
-                </MenuItem>
-                <MenuItem value={{quantity: "1",name: "Test 2", cost: "3.33", tags: "operations"}}>
-                    Test 2
-                </MenuItem>
-            </Select>
-            <Button className="btn bold">Hosting</Button>
-            <Button className="btn bold">Payroll</Button>
-        </Box>
-        <Box component="form" sx={{ padding: "8px" }}>
-            <div>
-                <TextField
-                    required
-                    id="quantity"
-                    label="Quantity"
-                    autoComplete="off"
-                    type="text"
-                    value={quantity}
-                    onChange={handleQuantity}
-                    error={badQuantity}
-                    onDoubleClick={() => { setQuantity("") }}
-                    sx={{ width: 1 / 4, margin: "8px 4px" }}
-                />
-                <TextField
-                    required
-                    id="name"
-                    label="Name of Purchase"
-                    value={name}
-                    onChange={handleName}
-                    error={badName}
-                    onDoubleClick={() => { setName("") }}
-                    sx={{ width: 4 / 6, margin: "8px 4px" }}
-                />
-            </div>
-            <div>
-                <TextField
-                    required
-                    id="cost"
-                    label="Cost"
-                    autoComplete="off"
-                    value={cost}
-                    onChange={handleCost}
-                    error={badCost}
-                    onDoubleClick={() => { setCost("") }}
-                    onBlur={handleCostBlur}
-                    sx={{ width: 1 / 4, margin: "8px 4px" }}
-                />
-                <TextField
-                    id="tags"
-                    label="Tags"
-                    helperText="Separate with commas"
-                    value={tags}
-                    onChange={handleTags}
-                    onDoubleClick={() => { setTags("") }}
-                    sx={{ width: 4 / 6, margin: "8px 4px" }}
-                />
-            </div>
-            <div>
+            <Box sx={{ padding: "8px " }}>
+                <Divider sx={{ marginTop: "16px" }} textAlign="left">Presets</Divider>
+                <InputLabel id="preset-select-label">Presets</InputLabel>
+                <Select
+                    labelId="preset-select-label"
+                    id="preset-purchase"
+                    label="Presets"
+                    value={""}
+                    sx={{ width: "200px" }}
+                    onChange={handlePreset}
+                >
+                    <MenuItem value={{ quantity: "", name: "Test 1", cost: "", tags: "operations" }}>
+                        Test 1
+                    </MenuItem>
+                    <MenuItem value={{ quantity: "1", name: "Test 2", cost: "3.33", tags: "operations" }}>
+                        Test 2
+                    </MenuItem>
+                </Select>
+                <Button className="btn bold">Hosting</Button>
+                <Button className="btn bold">Payroll</Button>
+            </Box>
+            <Box component="form" sx={{ padding: "8px" }}>
+                <Divider sx={{ marginTop: "16px" }} textAlign="left">Purchase Form</Divider>
+                <div>
+                    <TextField
+                        required
+                        id="quantity"
+                        label="Quantity"
+                        autoComplete="off"
+                        type="text"
+                        value={quantity}
+                        onChange={handleQuantity}
+                        error={badQuantity}
+                        onDoubleClick={() => { setQuantity("") }}
+                        sx={{ width: 1 / 4, margin: "8px 4px" }}
+                    />
+                    <TextField
+                        required
+                        id="name"
+                        label="Name of Purchase"
+                        value={name}
+                        onChange={handleName}
+                        error={badName}
+                        onDoubleClick={() => { setName("") }}
+                        sx={{ width: 4 / 6, margin: "8px 4px" }}
+                    />
+                </div>
+                <div>
+                    <TextField
+                        required
+                        id="cost"
+                        label="Cost"
+                        autoComplete="off"
+                        value={cost}
+                        onChange={handleCost}
+                        error={badCost}
+                        onDoubleClick={() => { setCost("") }}
+                        onBlur={handleCostBlur}
+                        sx={{ width: 1 / 4, margin: "8px 4px" }}
+                    />
+                    <TextField
+                        id="tags"
+                        label="Tags"
+                        helperText="Separate with commas"
+                        value={tags}
+                        onChange={handleTags}
+                        onDoubleClick={() => { setTags("") }}
+                        sx={{ width: 4 / 6, margin: "8px 4px" }}
+                    />
+                </div>
+            </Box>
+            <Box sx={{ alignContent: "right", marginTop: "16px", padding:"8px" }}>
                 <Button disabled={isAnyBadInput()} onClick={handleSubmit} className="btn bold">Submit</Button>
                 <Button disabled={isAnyBadInput() && isNewPreset()} className="btn bold">Save</Button>
-                <Button onClick={resetForm} className="btn bold">Cancel</Button>    
-            </div>
-        </Box>
+                <Button onClick={resetForm} className="btn bold">Cancel</Button>
+            </Box>
         </>
     );
 }
