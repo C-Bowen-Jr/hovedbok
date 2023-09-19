f845de4d25b943f0f34bafd89d518068530931ec
Chris Bowen Jr <laylong@gmx.com>
2023-09-19 17:58:17 -0400

    Tauri Things Feature:  - Groundwork to replace nav menus with native menus

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 #[tauri::command]
 fn update_save_file(invoke_message: String) {
@@ -7,7 +8,30 @@ fn update_save_file(invoke_message: String) {
 } 
 
 fn main() {
-  tauri::Builder::default()
+    let file_menu = Submenu::new("File", Menu::new()
+      .add_item(CustomMenuItem::new("newproduct", "New Product"))
+      .add_item(CustomMenuItem::new("editproduct", "Edit Product"))
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Quit));
+    let hovedbok_menu = Submenu::new("Hovedbok", Menu::new()
+      .add_item(CustomMenuItem::new("help", "Help"))
+      .add_item(CustomMenuItem::new("about", "About")));
+    let menu = Menu::new()
+      .add_submenu(file_menu)
+      .add_submenu(hovedbok_menu);
+    tauri::Builder::default()
+    .menu(menu)
+    .on_menu_event(|event| {
+      match event.menu_item_id() {
+        "quit" => {
+          std::process::exit(0);
+        }
+        "close" => {
+          event.window().close().unwrap();
+        }
+        _ => {}
+      }
+    })
     .invoke_handler(tauri::generate_handler![update_save_file])
     .run(tauri::generate_context!())
     .expect("Error while running tauri application");
