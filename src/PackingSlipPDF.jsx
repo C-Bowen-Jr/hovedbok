c57b20d8eb774bfb7ceb4e2147395486a749128a
Chris Bowen Jr <laylong@gmx.com>
2023-10-27 21:19:13 -0400

    Further PDF Tweaks Feature:  - Added date and order number  - Added addresses and company info

--- src/PackingSlipPDF.jsx
+++ src/PackingSlipPDF.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { PDFViewer, Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+import { format_date } from './utils.js';
 
 // Create styles
 const styles = StyleSheet.create({
@@ -12,7 +14,7 @@ const styles = StyleSheet.create({
     head_section: {
         margin: 10,
         padding: 10,
-        flexGrow: 1,
+        width: 280,
         minHeight: 250,
         border: 1
     },
@@ -22,21 +24,50 @@ const styles = StyleSheet.create({
         minHeight: 550,
         border: 1
     }
+
 });
+
+const LabeledText = props => (
+    <View style={{ fontFamily: 'Times-Bold' }}>
+        <Text>
+            {props.boldText}: {"\u0020"}
+            <Text style={{ fontFamily: 'Times-Roman' }}>
+                {props.children}
+            </Text>
+        </Text>
+        
+    </View>
+);
 
 // Create Document Component
 export default function PrintPreview() {
+    const currentOrderNumber = useSelector((state) => state.currentOrderNumber);
+    const todaysDate = new Date();
+
     return (
-        <PDFViewer style={{ width: 600, height: 860 }}>
+        <PDFViewer debug={true} style={{ width: 600, height: 860 }}>
             <Document>
                 <Page height={101} width={152} style={styles.page}>
                     <View style={styles.header}>
                         <View style={styles.head_section}>
-                            <Text>Section #1</Text>
-                            <Text>This is line 2?</Text>
+                            <Text style={{ fontFamily: 'Times-Bold', fontSize: 22}}>COMPANY NAME</Text>
+                            <Text>www.company-website.com</Text>
+                            <Text style={{ fontSize: 78 }}> </Text>
+                            <LabeledText boldText={'Ship From'}></LabeledText>
+                            <Text>Company Name</Text>
+                            <Text>Address Line 1</Text>
+                            <Text>Address Line 2</Text>
+                            <Text>City, State 12345</Text>
                         </View>
                         <View style={styles.head_section}>
-                            <Text>Section #2</Text>
+                            <LabeledText boldText={'Date'}>{format_date(todaysDate)}</LabeledText>
+                            <LabeledText boldText={'Order'}>{currentOrderNumber}</LabeledText>
+                            <Text> </Text>
+                            <LabeledText boldText={'Ship To'}></LabeledText>
+                            <Text>Customer Name</Text>
+                            <Text>Address Line 1</Text>
+                            <Text>Address Line 2</Text>
+                            <Text>City, State 12345</Text>
                         </View>
                     </View>
                     <View style={styles.body}>
